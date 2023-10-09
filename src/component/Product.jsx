dd14076a18dd0bec5fe79c5a696b443b6961265f
SDHORSE94 <126835167+SDHORSE94@users.noreply.github.com>
2023-10-09 17:31:14 -0400

    c

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -1,57 +1,52 @@
-import React,{useState,useEffect} from 'react'
-import '../App.css'
-import axios from 'axios'
-
-
+import React, { useState, useEffect } from "react";
+import "../App.css";
+import axios from "axios";
 
 const Product = () => {
-  const[loading,setloading]= useState(false);
-  const [data,setData] = useState([]);
+  const [loading, setloading] = useState(false);
+  const [data, setData] = useState([]);
 
-useEffect(()=>{
-  setloading(true);
+  useEffect(() => {
+    setloading(true);
 
-  axios({
-    method:"GET",
-    url:"https://fakestoreapi.com/products",
+    axios({
+      method: "GET",
+      url: "https://fakestoreapi.com/products",
+    })
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((e) => console.log(e))
+      .finally(() => setloading(false));
+  }, []);
 
-  }).then(res=> {
-    console.log(res.data);
-    setData(res.data);
-  })
-  .catch((e)=>console.log(e))
-  .finally(()=>setloading(false));
-},[]);
-  
-
-
-
-  return(
-
-   <div className="products-container">
-    {loading && (
-    <div>
-      {""}
-       <h1>Loading....</h1>
-    </div>
-    )}
-
-    {data.map((product)=>(
-      <div key={product.id} className="card">
-        <div><img src={product.image} alt="#"/></div>
-        <div className="card-description">
-          <h6>{product.title}</h6>
-          <h6>{`Price: ${product.price}`}</h6>
-          <h6>{`Category: ${product.category}`}</h6>
+  return (
+    <div className="products-container">
+      {loading && (
+        <div>
+          {""}
+          <h1>Loading....</h1>
         </div>
+      )}
 
-      </div>
-
-    ))}
+      {data.map((product) => (
+        <div key={product.id} className="card">
+          <div>
+            <img src={product.image} alt="#" />
+          </div>
+          <div className="card-description">
+            <h6>{product.title}</h6>
+            <h6>{`Price: ${product.price}`}</h6>
+            <h6>{`Category: ${product.category}`}</h6>
+            <div>
+              <button>Add to Cart</button>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
-  );  
-  };
-    
-
+  );
+};
 
 export default Product;
